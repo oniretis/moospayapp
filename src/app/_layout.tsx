bebf4fec93408f14941d75d4b24e4f1275f5e527
oniretis <oluwatunbo@gmail.com>
2026-04-01 16:08:40 +0100

    package setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,13 @@
 import "../global.css";
 
 import { Slot } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { HeroUINativeProvider } from "heroui-native";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider><Slot /></HeroUINativeProvider>
+    </GestureHandlerRootView>
+  )
 }
